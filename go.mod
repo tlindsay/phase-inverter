76ac095ca339691eefb72aad952586b8df3d1eda
Patrick Lindsay <pat@thatdarnpat.com>
2024-07-09 22:41:34 -0500

    Shit is working!

--- go.mod
+++ go.mod
@@ -1,10 +1,11 @@
 module github.com/tlindsay/phase-inverter
 
-go 1.22.5
+go 1.21.4
 
 require (
 	github.com/charmbracelet/log v0.4.0
 	github.com/eclipse/paho.mqtt.golang v1.4.3
+	github.com/getlantern/systray v1.2.2
 	github.com/tlindsay/subspace v0.0.0-20231026235810-4fde8eb28085
 	golang.design/x/hotkey v0.4.1
 )
@@ -12,13 +13,21 @@ require (
 require (
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/charmbracelet/lipgloss v0.10.0 // indirect
+	github.com/getlantern/context v0.0.0-20190109183933-c447772a6520 // indirect
+	github.com/getlantern/errors v0.0.0-20190325191628-abdb3e3e36f7 // indirect
+	github.com/getlantern/golog v0.0.0-20190830074920-4ef2e798c2d7 // indirect
+	github.com/getlantern/hex v0.0.0-20190417191902-c6586a6fe0b7 // indirect
+	github.com/getlantern/hidden v0.0.0-20190325191715-f02dbb02be55 // indirect
+	github.com/getlantern/ops v0.0.0-20190325191751-d70cb0d6f85f // indirect
 	github.com/go-logfmt/logfmt v0.6.0 // indirect
+	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.18 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/muesli/reflow v0.3.0 // indirect
 	github.com/muesli/termenv v0.15.2 // indirect
+	github.com/oxtoacart/bpool v0.0.0-20190530202638-03653db5a59c // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
 	golang.design/x/mainthread v0.3.0 // indirect
 	golang.org/x/exp v0.0.0-20231006140011-7918f672742d // indirect
--- main.go
+++ main.go
@@ -1,10 +1,69 @@
 package main
 
 import (
-	"github.com/tlindsay/phase-inverter/helm"
+	"os"
+
+	"github.com/charmbracelet/log"
+	"github.com/getlantern/systray"
+	"golang.design/x/hotkey"
+	"golang.design/x/hotkey/mainthread"
 )
 
+type PhaseInverter struct {
+	VolDown *hotkey.Hotkey
+	VolUp   *hotkey.Hotkey
+}
+
 func main() {
-	helm.Engage()
-	// transmitter.Transmit()
+	pi := &PhaseInverter{}
+	systray.Run(pi.onScreen, pi.endTransmission)
+}
+
+func (pi *PhaseInverter) RegisterHotkeys() {
+	hkVolDown := hotkey.New([]hotkey.Modifier{hotkey.ModCmd, hotkey.ModShift}, hotkey.KeyDown)
+	if err := hkVolDown.Register(); err != nil {
+		log.Fatalf("hotkey: failed to register hotkey: %v", err)
+	}
+	log.Infof("hotkey: %v is registered", hkVolDown)
+
+	hkVolUp := hotkey.New([]hotkey.Modifier{hotkey.ModCmd, hotkey.ModShift}, hotkey.KeyUp)
+	if err := hkVolUp.Register(); err != nil {
+		log.Fatalf("hotkey: failed to register hotkey: %v", err)
+	}
+	log.Infof("hotkey: %v is registered", hkVolUp)
+
+	pi.VolDown = hkVolDown
+	pi.VolUp = hkVolUp
+}
+
+func (pi *PhaseInverter) onScreen() {
+	icon, err := os.ReadFile("./assets/icon.png")
+	if err != nil {
+		log.Fatalf("viewer: error reading icon: %v", err)
+	}
+	systray.SetTemplateIcon(icon, icon)
+	quitItem := systray.AddMenuItem("Quit", "Quit")
+
+	go mainthread.Init(func() {
+		pi.RegisterHotkeys()
+		for {
+			select {
+			case <-quitItem.ClickedCh:
+				systray.Quit()
+			case <-pi.VolDown.Keydown():
+				log.Infof("phase inverter: VolDown keypress")
+			case <-pi.VolUp.Keydown():
+				log.Infof("phase inverter: VolUp keypress")
+			}
+		}
+	})
+
+}
+
+func (pi *PhaseInverter) endTransmission() {
+	log.Infof("viewer: Shutting down...")
+	pi.VolDown.Unregister()
+	log.Infof("hotkey: %v is unregistered", pi.VolDown)
+	pi.VolUp.Unregister()
+	log.Infof("hotkey: %v is unregistered", pi.VolUp)
 }
